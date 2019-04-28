896ebca5ab960a7fa319c3ab8413b31266cf6e37
Dennis_Njogu <dcolonel6@gmail.com>
2019-04-28 19:23:30 +0300

    added an event listener 'input' on input tag

--- js/compareTriangles.js
+++ js/compareTriangles.js
@@ -1,7 +1,24 @@
 
 (function(){
     //we will initialise the obj will DOM elements
+    let init = {
+        input: document.querySelector("input"),
+        button: document.querySelector("button"),
+        ul: document.querySelector("ul")
+    };
     
+    init.button.addEventListener("click",getInput);
+    function getInput(evt){    
+        let dimensions = document.getElementById("dimensions").value,arrayDimensions = [];
+        arrayDimensions = dimensions.split(/\s+/).map(Number);
+        console.log(compare(...arrayDimensions));    
+    };
+    init.input.addEventListener("input",(evt)=>{
+        console.log(evt.target.value);
+        //run validate methods
+    })
+    
+
 
 
 
