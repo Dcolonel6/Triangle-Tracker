308ce12953bcbdd27371e051b09dccedb64e48b3
Dennis_Njogu <dcolonel6@gmail.com>
2019-04-28 19:44:49 +0300

    added a ffunction that validates input

--- js/compareTriangles.js
+++ js/compareTriangles.js
@@ -8,15 +8,25 @@
     };
     
     init.button.addEventListener("click",getInput);
+    /***
+     * click handler for button
+     */
     function getInput(evt){    
         let dimensions = document.getElementById("dimensions").value,arrayDimensions = [];
         arrayDimensions = dimensions.split(/\s+/).map(Number);
         console.log(compare(...arrayDimensions));    
     };
+
     init.input.addEventListener("input",(evt)=>{
         console.log(evt.target.value);
         //run validate methods
     })
+    /***
+     * function that will compare values and determine what type 
+     * or if its a triangle
+     * @params: type number, 
+     * return value :string
+     */
     function compare(d1,d2,d3){
         let triangle = new Triangle(d1,d2,d3);
           
@@ -37,6 +47,14 @@
         else{
             return "This is not A triangle";
         }
+    }
+    /*** function that will validate input
+     * @params: type string, 
+     * return value :boolean
+     */
+    function validate(input){
+        
+        return /\d+\s+\d+\s+\d+/.test(input);
     }
 
 
