de8bafc03f7b776e50e4ca9c77553d35df86e106
Dennis_Njogu <dcolonel6@gmail.com>
2019-04-28 19:24:57 +0300

    added a ffunction that checks type of a triangle

--- js/compareTriangles.js
+++ js/compareTriangles.js
@@ -17,7 +17,27 @@
         console.log(evt.target.value);
         //run validate methods
     })
-    
+    function compare(d1,d2,d3){
+        let triangle = new Triangle(d1,d2,d3);
+          
+        
+        if(triangle.isTriangle()){
+                //find out what type
+                
+            if(triangle.isEquilateral()){
+                return "This is an equilateral triangle";
+            }
+            else if (triangle.isIsosceles()){
+                return "This is a Isosceles";
+            }
+            else {
+                return "This is a Scalene";
+            }
+        }
+        else{
+            return "This is not A triangle";
+        }
+    }
 
 
 
