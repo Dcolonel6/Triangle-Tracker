47fd5b3b52f54e6ba988fcf7447690974d2a7d05
Dennis_Njogu <dcolonel6@gmail.com>
2019-04-28 19:21:28 +0300

    refactored module CompareTriangle to use an IIFE

--- js/compareTriangles.js
+++ js/compareTriangles.js
@@ -1,27 +1,9 @@
-function compare(d1,d2,d3){
-    let triangle = new Triangle(d1,d2,d3);
-  
 
-    if(triangle.isTriangle()){
-        //find out what type
-        
-        if(triangle.isEquilateral()){
-            return "This is an equilateral triangle";
-        }
-        else if (triangle.isIsosceles()){
-            return "This is a Isosceles";
-        }
-        else {
-            return "This is a Scalene";
-        }
-    }
-    else{
-        return "This is not A triangle";
-    }
-}
-function getInput(evt){    
-    let dimensions = document.getElementById("dimensions").value,arrayDimensions = [];
-    arrayDimensions = dimensions.split(/\s+/).map(Number);
-    console.log(compare(...arrayDimensions));
+(function(){
+    //we will initialise the obj will DOM elements
     
-}
+
+
+
+
+})()
